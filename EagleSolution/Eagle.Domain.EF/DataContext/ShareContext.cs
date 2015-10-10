0f1e28dee05caa43c3831af5af44233e075a7af9
key-value <yinyazuolun@sina.com>
2015-10-10 18:09:13 +0800

    添加分享库

---
+++ EagleSolution/Eagle.Domain.EF/DataContext/ShareContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Reflection;
+using Eagle.Domain.Core.Model;
+using Eagle.Domain.EF.Map.Share;
+using Eagle.Domain.EF.Migrations.Share;
+using Eagle.Model.Share;
+
+namespace Eagle.Domain.EF.DataContext
+{
+    public class ShareContext : DbContext
+    {
+        static ShareContext()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ShareContext, Configuration>());
+            //Database.SetInitializer<DefaultContext>(null);
+
+#if DEBUG
+            DbInterception.Add(new EFIntercepterLogging());
+#endif
+        }
+
+        public ShareContext()
+            : base("ShareConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.ValidateOnSaveEnabled = false;
+            //this.Configuration.UseDatabaseNullSemantics = false;
+        }
+
+        public DbSet<ShareTree> Trees { get; set; }
+
+        public void ModifiedModel(IEntity entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetAssembly(typeof (TreeMap)));
+        }
+    }
+}
---
+++ EagleSolution/Eagle.Domain.EF/Map/Share/TreeMap.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Data.Entity.ModelConfiguration;
+using Eagle.Model;
+using Eagle.Model.Share;
+
+namespace Eagle.Domain.EF.Map.Share
+{
+    public class TreeMap : EntityTypeConfiguration<ShareTree>
+    {
+        /// <summary>
+        /// Initializes a new instance of EntityTypeConfiguration
+        /// </summary>
+        public TreeMap()
+        {
+            this.HasKey(x => x.ID);
+        }
+    }
+}
