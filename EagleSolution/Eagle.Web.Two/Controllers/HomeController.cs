54f05e70c2e986744e488cb067ca28b001eb484c
key-value <yinyazuolun@sina.com>
2015-11-26 20:05:18 +0800

    加入测试站点

---
+++ EagleSolution/Eagle.Web.Two/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Eagle.Web.Two.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public class CommentModel
+        {
+            public string Author
+            {
+                get; set;
+            }
+            public string Text
+            {
+                get; set;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        private static IList<CommentModel> _comments = new List<CommentModel>()  {
+                new CommentModel
+                {
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+
+        [OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+    }
+}
---
+++ EagleSolution/Eagle.Web.Two/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eagle.Web.Two.MvcApplication" Language="C#" %>
